7814e5a9a0bb90def42f6c9207dcddecdcfdf109
Vangelis Giannakosian <vangelis@telesto.gr>
2021-12-27 20:59:58 +0200

    fixed routing issue

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -21,10 +21,10 @@ class App extends Component {
           <div>
             <Header />
             <Routes >
-              <Route path="/blogs/new" component={BlogNew} />
-              <Route exact path="/blogs/:_id" component={BlogShow} />
-              <Route path="/blogs" component={Dashboard} />
-              <Route path="/" component={Landing} />
+              <Route path="/blogs/new" element={<BlogNew/>} />
+              <Route exact path="/blogs/:_id" element={<BlogShow/>} />
+              <Route path="/blogs" element={<Dashboard/>} />
+              <Route path="/" element={<Landing/>} />
             </Routes >
           </div>
         </BrowserRouter>
